4444352e8b2fb2914615db9e6c965a0df4598df4
nathanbjenx <nathanjenx@gmail.com>
2017-04-15 18:48:38 +0100

    Revert "Merge pull request #2 from nathanbjenx/scoped_session"
    This reverts commit 45cc56527074905bf78b23fc2201f39b6a6bf7c3, reversing
    changes made to 200af2828653f0cc5fa679b75917b53e9ab9f085.

--- cairis/core/MySQLDatabaseProxy.py
+++ cairis/core/MySQLDatabaseProxy.py
@@ -333,8 +333,7 @@ class MySQLDatabaseProxy(DatabaseProxy.DatabaseProxy):
       if (closeConn) and self.conn.open:
         self.conn.close()
       if b.runmode == 'desktop':
-        dbEngine = create_engine('mysql+mysqldb://'+b.dbUser+':'+b.dbPasswd+'@'+b.dbHost+':'+str(b.dbPort)+'/'+b.dbName)
-        self.conn = scoped_session(sessionmaker(bind=dbEngine))
+        self.conn = MySQLdb.connect(host=b.dbHost,port=b.dbPort,user=b.dbUser,passwd=b.dbPasswd,db=b.dbName)
       elif b.runmode == 'web':
         ses_settings = b.get_settings(session_id)
         self.conn = MySQLdb.connect(host=ses_settings['dbHost'],port=ses_settings['dbPort'],user=ses_settings['dbUser'],passwd=ses_settings['dbPasswd'],db=ses_settings['dbName'])
@@ -11617,40 +11616,4 @@ class MySQLDatabaseProxy(DatabaseProxy.DatabaseProxy):
     except _mysql_exceptions.DatabaseError, e:
       id,msg = e
       exceptionText = 'MySQL error creating CAIRIS database ' + dbName + '(id:' + str(id) + ',message:' + msg
-      raise DatabaseProxyException(exceptionText) 
-
-  def getUseCaseRequirements(self,ucName):
-    try:
-      curs = self.conn.connection().connection.cursor()
-      curs.execute('call useCaseRequirements(%s)',[ucName])
-      if (curs.rowcount == -1):
-        exceptionText = 'Error getting requirements associated with use case ' + ucName
-        raise DatabaseProxyException(exceptionText) 
-      reqs = [] 
-      for row in curs.fetchall():
-        row = list(row)
-        reqs.append(row[0])
-      curs.close()
-      return reqs
-    except _mysql_exceptions.DatabaseError, e:
-      id,msg = e
-      exceptionText = 'MySQL error getting requirements associated with use case ' + ucName + '  (id:' + str(id) + ',message:' + msg + ')'
-      raise DatabaseProxyException(exceptionText) 
-
-  def getUseCaseGoals(self,ucName,envName):
-    try:
-      curs = self.conn.connection().connection.cursor()
-      curs.execute('call useCaseGoals(%s,%s)',[ucName,envName])
-      if (curs.rowcount == -1):
-        exceptionText = 'Error getting goals associated with use case ' + ucName
-        raise DatabaseProxyException(exceptionText) 
-      goals = [] 
-      for row in curs.fetchall():
-        row = list(row)
-        goals.append(row[0])
-      curs.close()
-      return goals
-    except _mysql_exceptions.DatabaseError, e:
-      id,msg = e
-      exceptionText = 'MySQL error getting goals associated with use case ' + ucName + '  (id:' + str(id) + ',message:' + msg + ')'
       raise DatabaseProxyException(exceptionText) 
